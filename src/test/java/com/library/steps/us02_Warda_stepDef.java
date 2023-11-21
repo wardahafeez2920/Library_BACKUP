549ef24be09cf1fe88587eeb5595fcb7d7476282
wardahafeez <hafeez.warda@gmail.com>
2023-11-20 23:00:29 -0500

    Finished US02

--- src/test/java/com/library/steps/us02_Warda_stepDef.java
+++ src/test/java/com/library/steps/us02_Warda_stepDef.java
@@ -9,18 +9,18 @@ import org.junit.Assert;
 public class us02_Warda_stepDef {
 
 BorrowedBooksPage borrowedBooksPage = new BorrowedBooksPage();
-    String expectedBorrowedBooks = "";
+    String actualBorrowedBooks = "";
 
     @When("the librarian gets borrowed books number-WH")
     public void the_librarian_gets_borrowed_books_number_wh() {
-         expectedBorrowedBooks = borrowedBooksPage.borrowedBooksCount.getText();
+        actualBorrowedBooks = borrowedBooksPage.borrowedBooksCount.getText();
     }
     @Then("borrowed books number information must match with DB-WH")
     public void borrowed_books_number_information_must_match_with_db_wh() {
    DB_Util.runQuery("select count(*) from book_borrow\n" +
             "where returned_date is null");
-   String actualBorrowedBooks = DB_Util.getFirstRowFirstColumn();
-        Assert.assertEquals(actualBorrowedBooks, expectedBorrowedBooks);
+   String expectedBorrowedBooks = DB_Util.getFirstRowFirstColumn();
+        Assert.assertEquals(expectedBorrowedBooks, actualBorrowedBooks);
     }
 
 
